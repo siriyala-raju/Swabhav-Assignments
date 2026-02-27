bdd9175a56317e7d540c1eb759317402b6e8d066
siriyala-raju <rajusiriyala44@gmail.com>
2026-02-27 21:09:17 +0530

    Renamed problem folders and Assignment1 to Assignments

--- Assignment1/P5_Fibonacci/Program.cs
+++ Assignment1/P5_Fibonacci/Program.cs
@@ -7,7 +7,7 @@
             
             //Console.Write("Enter a Number: ");
             //int input = int.Parse(Console.ReadLine());
-            //In problem statement you mentioned 8 thats why direclty I took 8 as count. If user input count = input
+            //In problem statement you mentioned 8 thats why direclty I took 8 as count. If user input it will be count = input
             int count = 8;
             int first = 0;
             int second = 1;
