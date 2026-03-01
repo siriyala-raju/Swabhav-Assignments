bf42e4d9829cd28b2125041d836f7f7805258522
siriyala-raju <rajusiriyala44@gmail.com>
2026-03-01 22:55:55 +0530

    Added Palindrome String Assignment

---
+++ Assignments/PalindromeString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PalindromeString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String: ");
+            string input = Console.ReadLine();
+
+            string word = input.ToLower();
+            bool isPalindrome = true;
+
+            for (int i = 0; i < word.Length / 2 ; i++)
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            if (isPalindrome)
+            {
+                Console.WriteLine($"{word} is a Palindrome String");
+            }
+            else 
+            {
+                Console.WriteLine($"{word} is not a Palindrome String");
+            }
+
+        }
+    }
+}
