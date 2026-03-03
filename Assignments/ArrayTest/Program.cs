f6bf332ef3350aedad7c2713c0f8f86c98a89ecf
siriyala-raju <rajusiriyala44@gmail.com>
2026-03-03 18:13:30 +0530

    Class Diagrams and Array & Loop Tests assignments added

---
+++ Assignments/ArrayTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArrayTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Raju", "Murugeesh", "Kamal", "Rohan" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Index: {i}  Value: {names[i]}");
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Value: {name}");
+            }
+        }
+    }
+}
---
+++ Assignments/Coding standards/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoopTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Raju");
+            }
+
+            int j = 1;
+            while (j <= 5)
+            {
+                Console.WriteLine("Raju");
+                j++;
+            }
+        }
+    }
+}
