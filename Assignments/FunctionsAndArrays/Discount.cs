9257ca0592ecf7a72ffbd6aa7591a537292835e4
siriyala-raju <rajusiriyala44@gmail.com>
2026-03-09 14:44:55 +0530

    Added FunctionsAndArrays

---
+++ Assignments/FunctionsAndArrays/Discount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionsAndArrays
+{
+    internal class Discount
+    {
+        public double CalculateDiscount(double fee)
+        {
+            if (fee > 7000)
+            {
+                fee = fee - (fee * 0.05);
+            }
+            return fee;
+        }
+    }
+}
---
+++ Assignments/FunctionsAndArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FunctionsAndArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] fees = new double[5];
+            Discount obj = new Discount();
+
+            for (int i = 0; i < fees.Length; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Enter fees for Student {i + 1} : ");
+                    double fee = Convert.ToDouble(Console.ReadLine());
+
+                    if (fee < 5000 || fee > 10000)
+                    {
+                        Console.WriteLine("Fees must be in the range 5000 to 10000");
+                    }
+                    else
+                    {
+                        fees[i] = fee;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < fees.Length; i++)
+            {
+                double finalFee = obj.CalculateDiscount(fees[i]);
+                Console.WriteLine($"Student {i + 1} : {finalFee}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
