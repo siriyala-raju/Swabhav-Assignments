f63c3ac825da368048b7a7081d685415ceead7b1
siriyala-raju <rajusiriyala44@gmail.com>
2026-03-10 16:20:05 +0530

    Added Student Grading

---
+++ Assignments/StudentGrading/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentGrading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = { 60, 70, 75, 80, 88 };
+
+            Student student1 = new Student("Swapnil Kulkarni", "R102", marks);
+
+            Console.WriteLine(student1);
+        }
+    }
+}
---
+++ Assignments/StudentGrading/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml;
+
+namespace StudentGrading
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string RollNo { get; set; }
+        public int[] Marks { get; set; }
+
+        public Student(string name, string rollNo, int[] marks)
+        {
+            this.Name = name;
+            this.RollNo = rollNo;
+            this.Marks = marks;
+        }
+
+        public double CalculateAverage()
+        {
+            int sum = 0;
+            foreach (int score in Marks)
+            {
+                sum += score;
+            }
+            double average = sum / 5.0;
+            return average;
+        }
+
+        public string CalculateGrade()
+        {
+            double average = CalculateAverage();
+            if (average >= 90)
+                return "A+";
+            else if (average >= 80)
+                return "A";
+            else if (average >= 70)
+                return "B";
+            else if (average >= 60)
+                return "C";
+            else if (average >= 50)
+                return "D";
+            else
+                return "Fail";
+        }
+        public override string ToString()
+        {
+            string marksList = "[";
+
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                marksList += Marks[i];
+
+                if (i < Marks.Length - 1)
+                    marksList += ", ";
+            }
+
+            marksList += "]";
+
+            return $"Student Report Card\n" +
+                   $"----------------------------\n" +
+                   $"Name        : {Name}\n" +
+                   $"Roll No     : {RollNo}\n" +
+                   $"Marks       : {marksList}\n" +
+                   $"Average     : {CalculateAverage():0.0}\n" +
+                   $"Grade       : {CalculateGrade()}";
+        }
+    }
+}
