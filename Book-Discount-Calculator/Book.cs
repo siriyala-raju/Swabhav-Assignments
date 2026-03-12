eb8bd062d79f4d023967130572024bd7a834d2d7
siriyala-raju <rajusiriyala44@gmail.com>
2026-03-12 17:02:58 +0530

    Added Book-Discount-Calculator

---
+++ Book-Discount-Calculator/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_Discount_Calculator
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public double Price;
+        public bool IsBestseller;
+
+        public Book(string title, string author, double price, bool isBestseller)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Price = price;
+            this.IsBestseller = isBestseller;
+        }
+
+        public double DiscountedPrice()
+        {
+            double finalPrice = Price;
+
+            if (IsBestseller)
+            {
+                Console.WriteLine($"'{Title}' is a bestseller. Applying 10% discount.");
+                finalPrice = finalPrice - (finalPrice * 0.10);
+            }
+
+            if (Price > 500)
+            {
+                Console.WriteLine($"'{Title}' is priced over 500. Applying additional 5% discount.");
+                finalPrice = finalPrice - (finalPrice * 0.05);
+            }
+            return finalPrice;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Original Price: ${Price}");
+
+            double discounted = DiscountedPrice();
+            Console.WriteLine($"Final Discounted Price: ${discounted}");
+        }
+    }
+}
---
+++ Book-Discount-Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Book_Discount_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("The Power of Habit", "Charles Duhigg", 600, true);
+            Book b2 = new Book("C# Programming", "Jane Doe", 450, false);
+
+            b1.Display();
+            b2.Display();
+        }
+    }
+}
