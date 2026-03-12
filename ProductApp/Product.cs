4c4eae80200dd357a5ca533b2c001a5cebaf0219
siriyala-raju <rajusiriyala44@gmail.com>
2026-03-12 16:19:36 +0530

    Added ProductApp

---
+++ ProductApp/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp
+{
+    internal class Product
+    {
+        public int id;
+        public string name;
+        public double price;
+        public double discountPercentage;
+
+        public Product(int id, string name, double price, double discountPercentage)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.discountPercentage = discountPercentage;
+        }
+
+        public double DiscountedPrice()
+        {
+            double discountAmount = price * discountPercentage / 100;
+            return price - discountAmount;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Product ID: {id}");
+            Console.WriteLine($"Product Name: {name}");
+            Console.WriteLine($"Actual Price: {price}");
+            Console.WriteLine($"Discount Percentage: {discountPercentage} %");
+            Console.WriteLine($"Price After Discount: {DiscountedPrice()}");
+        }
+    }
+}
---
+++ ProductApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product(1, "Laptop", 50000, 10);
+            Product p2 = new Product(2, "Mobile", 20000, 5);
+
+            p1.Display();
+            p2.Display();
+        }
+    }
+}
