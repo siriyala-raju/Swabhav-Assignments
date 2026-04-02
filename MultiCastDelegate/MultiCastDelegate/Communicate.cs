d0eaa62040a37eaa6367907804fd564b44c92c92
siriyala-raju <rajusiriyala44@gmail.com>
2026-04-03 00:26:11 +0530

    Added Multi Cast Delegate

---
+++ MultiCastDelegate/MultiCastDelegate/Communicate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiCastDelegate
+{
+    internal class Communicate
+    {
+        public delegate void Notify(string message);
+
+        public static void SendEmail(string message)
+        {
+            Console.WriteLine("Email Sent: " + message);
+        }
+
+        public static void SendSMS(string message)
+        {
+            Console.WriteLine("SMS Sent: " + message);
+        }
+
+        public static void SendWhatsApp(string message)
+        {
+            Console.WriteLine("WhatsApp Sent: " + message);
+        }
+    }
+}
---
+++ MultiCastDelegate/MultiCastDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using static MultiCastDelegate.Communicate;
+
+namespace MultiCastDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notify notify = null;
+            notify += SendEmail;
+            notify += SendSMS;
+            notify += SendWhatsApp;
+
+            notify("Order Placed");
+
+            Console.WriteLine("\n---- After Removing SMS ----\n");
+
+            notify -= SendSMS;
+
+            notify("Order Shipped");
+        }
+    }
+}
