a1040e7edc5a0830987cc85696d45d94e4b59319
siriyala-raju <rajusiriyala44@gmail.com>
2026-04-03 00:19:40 +0530

    Added FuncDelegate

---
+++ FuncDelegate/FuncDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> calculateDiscount = price => price > 1000 ? price * 0.15 : price * 0.05;
+            Func<double, double, double> calculateFinalPrice = (price, discount) => price - discount;
+
+            Console.Write("Enter product price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            double discount = calculateDiscount(price);
+            double finalPrice = calculateFinalPrice(price, discount);
+
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Discount: " + discount);
+            Console.WriteLine("Final Price: " + finalPrice);
+        }
+    }
+}
